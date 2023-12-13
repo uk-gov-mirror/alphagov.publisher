f7df0b59365a806bf1b4cb55c325951a6da98f83
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2023-12-13 09:00:07 +0000

    Hide diagram initially
    Render a loading message initially on the simple smart answer diagram visualisation page, and then switch out the diagram once mermaid has been initialised, in an attempt to mitigate an issue whereby the mermaid diagram source is briefly visible before the diagram is rendered.

--- app/assets/javascripts/modules/smart_answer_flowchart.js
+++ app/assets/javascripts/modules/smart_answer_flowchart.js
@@ -3,7 +3,13 @@
   'use strict'
   Modules.SmartAnswerFlowchart = function () {
     this.start = function (element) {
+      var diagram = element.find('.flowchart');
+      var flowchart__loading = element.find('.flowchart__loading');
+
       mermaid.initialize({ startOnLoad: true, flowchart: { useMaxWidth: false }});
+
+      diagram.removeClass('flowchart--hidden')
+      flowchart__loading.addClass('flowchart__loading--hidden')
     }
   }
 })(window.GOVUKAdmin.Modules)

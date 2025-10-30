6ff5497a57dc42ab455ded0a7673a03a1dbf8d6b
davidtrussler <mail@davidtrussler.net>
2025-10-30 13:02:48 +0000

    Update ga4_tracking_test integration test for data-attributes on form

--- test/integration/ga4_tracking_test.rb
+++ test/integration/ga4_tracking_test.rb
@@ -15,9 +15,25 @@ class Ga4TrackingTest < JavascriptIntegrationTest
     end
 
     should "render the correct ga4 data-attributes on the form" do
+      form = page.find("form")
+      form_ga4_event_data = JSON.parse(form["data-ga4-form"])
+
+      assert_equal form_ga4_event_data["action"], "Save"
+      assert_equal form_ga4_event_data["event_name"], "form_response"
+      assert_equal form_ga4_event_data["section"], "Edit publication"
+      assert_equal form_ga4_event_data["tool_name"], "publisher"
+      assert_equal form_ga4_event_data["type"], "edit"
+
+      assert page.has_css?("form[data-ga4-form-change-tracking]")
+      assert page.has_css?("form[data-ga4-form-record-json]")
+      assert page.has_css?("form[data-ga4-form-use-text-count]")
+    end
+
+    should "render the correct ga4 data-attributes on the form elements" do
       title_field = page.find("input[name='edition[title]']")
       metatag_field = page.find("textarea[name='edition[overview]']")
       body_field = page.find("textarea[name='edition[body]']")
+      # TODO: Find this element with a less generic find
       beta_field = page.find("fieldset")
 
       title_field_data = JSON.parse(title_field["data-ga4-index"])

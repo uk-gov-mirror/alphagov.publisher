c6be0e253eb23febac227faa6d3d85617ed8067e
davidtrussler <mail@davidtrussler.net>
2025-10-31 12:13:34 +0000

    Update ga4-form-setup module for changed data-attributes

--- app/assets/javascripts/analytics_modules/ga4-form-setup.js
+++ app/assets/javascripts/analytics_modules/ga4-form-setup.js
@@ -29,20 +29,21 @@ window.GOVUK.analyticsGa4.analyticsModules = window.GOVUK.analyticsGa4.analytics
     var eventData = {
       event_name: "form_response",
       type: "edit",
-      section: "Edit publication",
+      section: "Edit edition",
       action: "Save",
       tool_name: "publisher"
     }
 
     form.setAttribute('data-module', dataModule + ' ga4-form-tracker')
+    form.setAttribute('data-ga4-form-include-text', '')
     form.setAttribute('data-ga4-form-change-tracking', '')
     form.setAttribute('data-ga4-form-record-json', '')
-    form.setAttribute('data-ga4-form', JSON.stringify(eventData))
     form.setAttribute('data-ga4-form-use-text-count', '')
+    form.setAttribute('data-ga4-form', JSON.stringify(eventData))
   }
 
   Ga4FormSetup.prototype.callFormChangeTracker = function (form) {
-    const ga4FormTracker = new window.GOVUK.Modules.Ga4FormChangeTracker(form)
+    var ga4FormTracker = new window.GOVUK.Modules.Ga4FormChangeTracker(form)
 
     ga4FormTracker.init()
   }

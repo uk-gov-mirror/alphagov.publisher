9b8c45cdbc72014442c43e3096d974c6ed8b066a
davidtrussler <mail@davidtrussler.net>
2025-11-03 12:57:59 +0000

    Update ga4-index-section-setup unit test for indexing from 1 not 0

--- spec/javascripts/spec/ga4-index-section-setup.spec.js
+++ spec/javascripts/spec/ga4-index-section-setup.spec.js
@@ -32,17 +32,17 @@ describe('GA4IndexSectionSetup', function () {
     it('adds the correct data attributes to the form input elements', function () {
       // input [type="text"]
       var input1 = module.querySelector('#input_1')
-      expect(JSON.parse(input1.dataset.ga4Index).index_section).toBe(0)
+      expect(JSON.parse(input1.dataset.ga4Index).index_section).toBe(1)
       expect(JSON.parse(input1.dataset.ga4Index).index_section_count).toBe(3)
 
       // textarea
       var input2 = module.querySelector('#input_2')
-      expect(JSON.parse(input2.dataset.ga4Index).index_section).toBe(1)
+      expect(JSON.parse(input2.dataset.ga4Index).index_section).toBe(2)
       expect(JSON.parse(input2.dataset.ga4Index).index_section_count).toBe(3)
 
       // fieldset
       var input3 = module.querySelector('#input_3')
-      expect(JSON.parse(input3.dataset.ga4Index).index_section).toBe(2)
+      expect(JSON.parse(input3.dataset.ga4Index).index_section).toBe(3)
       expect(JSON.parse(input3.dataset.ga4Index).index_section_count).toBe(3)
     })
   })
